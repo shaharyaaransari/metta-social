e892e1f54a5d2a8d332ca8b03fc1733ef14979bb
shaharyaaransari <shaharyaaransari786@gmail.com>
2024-01-31 08:26:07 +0530

    pagination

--- currency-tracker/src/Component/CountriesCard.jsx
+++ currency-tracker/src/Component/CountriesCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function CountriesCard({name,capital,flags}) {
+export default function CountriesCard({name,capital,flags,population}) {
     
   return (
     <div className='countries-card'>
@@ -9,8 +9,9 @@ export default function CountriesCard({name,capital,flags}) {
        </div>
 
   <div>
-    <h4>Country: {name.common}</h4>
+    <h4>{name.common}</h4>
     <h4> Capital: {capital}</h4>
+    <h4>Population: {population}</h4>
   </div>
 
     </div>
--- currency-tracker/src/Countries.jsx
+++ currency-tracker/src/Countries.jsx
@@ -3,28 +3,55 @@ import CountriesCard from "./Component/CountriesCard";
 
 export const Countries = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
   const fetchData = () => {
+    setLoading(true);
     fetch(`https://restcountries.com/v3.1/all`)
       .then((res) => res.json())
       .then((res) => {
-     //   console.log(res);
-     setData(res)
+        setData(res);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
       });
   };
+
+  const selectPageHandler = (selectedPage) => {
+      if(selectedPage>=1 && selectedPage<=data.length/10 && selectedPage!==page)
+    setPage(selectedPage);
+  };
   useEffect(() => {
     fetchData();
   }, []);
+  if (loading) {
+    return <h1>Loading....</h1>;
+  }
   return (
-    <div className="countries">
-     {
-         data.length >0 && data.map((item)=>{
-   return <CountriesCard key={item.name} {...item}/>
-         })
-     }
-      
-    </div>
+    <>
+      <div className="countries">
+        {data.length > 0 &&
+          data.slice(page * 10 - 10, page * 10).map((item, index) => {
+            return <CountriesCard key={index} {...item} />;
+          })}
+      </div>
+      {data.length > 0 && (
+        <div className="pagination">
+          <span onClick={() => selectPageHandler(page- 1)} className={page > 1 ? "" : "pagination__disable"}>◀</span>
+          {[...Array(data.length / 10)].map((_, i) => {
+            return (
+              <span key={i} onClick={() => selectPageHandler(i + 1)}
+                className={page==i+1?"pagination_selected":""}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+
+          <span className={page < data.length / 10 ? "" : "pagination__disable"} onClick={() => selectPageHandler(page+ 1)}>▶</span>
+        </div>
+      )}
+    </>
   );
 };
